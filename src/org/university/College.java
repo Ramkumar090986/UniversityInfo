2806da608d61529affb9cab4b98359114240c195
Ramkumar090986 <ramkumar8687@gmail.com>
2020-11-23 19:11:16 +0530

    University Information

---
+++ src/org/university/College.java
@@ -0,0 +1,16 @@
+package org.university;
+
+public class College extends University {
+	public void ug() {
+		System.out.println("UG courses in SRM is Comaprable");
+
+	}
+	public void pg() {
+		System.out.println("PG courses in SRM is low");
+	}
+public static void main(String[] args) {
+	College c1=new College();
+	c1.ug();
+	c1.pg();
+}
+}
---
+++ src/org/university/University.java
@@ -0,0 +1,13 @@
+package org.university;
+
+public class University {
+	public void ug() {
+		System.out.println("UG courses in SRM is high");
+
+	}
+	public void pg() {
+		System.out.println("PG courses in SRM is best");
+
+	}
+
+}
