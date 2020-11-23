a2998fced1b041da335ac3184c12f249bdec0e94
Ramkumar090986 <ramkumar8687@gmail.com>
2020-11-23 19:48:54 +0530

    Unversity 2

--- src/org/university/College.java
+++ src/org/university/College.java
@@ -8,6 +8,10 @@ public class College extends University {
 	public void pg() {
 		System.out.println("PG courses in SRM is low");
 	}
+	public void physiotheraphy() {
+	System.out.println("Physiotherpahy");
+
+	}
 public static void main(String[] args) {
 	College c1=new College();
 	c1.ug();
